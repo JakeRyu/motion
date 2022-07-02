654718a3599ddcb8d5671f39ef2f12ae66cd63ff
JakeRyu <jake.ryu@barnett-waddingham.co.uk>
2022-07-02 19:12:25 +0100

    Add rest components

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import { ImageComponent } from './components/page/item/image.js';
 import { PageComponent } from "./components/page/page.js";
+import { NoteComponent } from './components/page/item/note.js';
+import { TodoComponent } from './components/page/item/todo.js';
+import { VideoComponent } from './components/page/item/video.js';
 
 class App{
     private readonly page: PageComponent;
@@ -9,6 +12,16 @@ class App{
 
         const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
         image.attachTo(appRoot, 'beforeend');
+
+        const video = new VideoComponent('Video title', 'https://www.youtube.com/watch?v=K3-jG52XwuQ');
+        video.attachTo(appRoot, 'beforeend');
+
+        const note = new NoteComponent('Note title', 'Note body');
+        note.attachTo(appRoot, 'beforeend');
+        
+        const todo = new TodoComponent('Todo title', 'Todo item');
+        todo.attachTo(appRoot, 'beforeend');
+
     }
 }
 
