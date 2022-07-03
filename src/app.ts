e4e8ede64df99262601e0452da83765350724318
JakeRyu <jake.ryu@barnett-waddingham.co.uk>
2022-07-03 10:01:47 +0100

    PageItemComponent to wrap page items

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,28 @@
 import { ImageComponent } from './components/page/item/image.js';
-import { PageComponent } from "./components/page/page.js";
+import { Composable, PageComponent } from "./components/page/page.js";
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
+import { Component } from './components/component.js';
 
 class App{
-    private readonly page: PageComponent;
+    private readonly page: Component & Composable;
+
     constructor(appRoot: HTMLElement){
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
 
         const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-        image.attachTo(appRoot, 'beforeend');
+        this.page.addChild(image);
 
         const video = new VideoComponent('Video title', 'https://www.youtube.com/watch?v=K3-jG52XwuQ');
-        video.attachTo(appRoot, 'beforeend');
+        this.page.addChild(video);
 
         const note = new NoteComponent('Note title', 'Note body');
-        note.attachTo(appRoot, 'beforeend');
-        
+        this.page.addChild(note);
+
         const todo = new TodoComponent('Todo title', 'Todo item');
-        todo.attachTo(appRoot, 'beforeend');
-    }
+        this.page.addChild(todo);    }
 }
 
 new App(document.querySelector('.document')!)
